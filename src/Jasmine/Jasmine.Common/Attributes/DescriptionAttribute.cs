b08fdee491d9785de2f2243ad7e9723c45c516c2
fuanlei <767550758@qq.com>
2019-06-04 10:01:08 +0800

    add file

---
+++ src/Jasmine/Jasmine.Common/Attributes/DescriptionAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Jasmine.Common.Attributes
+{
+    public class DescriptionAttribute:Attribute
+    {
+
+        public DescriptionAttribute(string des)
+        {
+            Description = des;
+        }
+        public string Description { get; }
+    }
+}
